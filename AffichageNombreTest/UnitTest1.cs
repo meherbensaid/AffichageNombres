09fc5e8635a80e3ca71bc177719effc01f28caf3
Meher ben said <Meher ben said>
2016-12-11 20:06:32 +0100

    Test Divisibilite 3 ,5 et 7

--- AffichageNombreTest/UnitTest1.cs
+++ AffichageNombreTest/UnitTest1.cs
@@ -1,14 +1,39 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using AffichageNombres;
 namespace AffichageNombreTest
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Divisibilite3()
         {
+            Nombre C = new Nombre();
+
+            int val = 9;
+            Boolean expected = true;
+            Assert.AreEqual(expected, C.SimpleDiv(val, 3));
+        }
+
+        [TestMethod]
+        public void Divisibilite5()
+        {
+            Nombre C = new Nombre();
+
+            int val = 20;
+            Boolean expected = true;
+            Assert.AreEqual(expected, C.SimpleDiv(val, 5));
+        }
+
+        [TestMethod]
+        public void Divisibilite7()
+        {
+            Nombre C = new Nombre();
+
+            int val = 28;
+            Boolean expected = true;
+            Assert.AreEqual(expected, C.SimpleDiv(val, 7));
         }
     }
 }
---
+++ AffichageNombres/Nombre.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AffichageNombres
+{
+    public class Nombre
+    {
+
+        public String trois { get; set; }
+        public String cinq { get; set; }
+        public String sept { get; set; }
+        String nombre = "";
+
+        public Nombre()
+        {
+            this.trois = "Foo";
+            this.cinq = "Bar";
+            this.sept = "Qix";
+
+        }
+
+        /// <summary>
+        ///methode qui retourne true si un nombre val et divisible par un nombre div
+        /// </summary>
+        /// <param name="val">nombre qu'on veut diviser</param>
+        /// <param name="div">diviseur</param>
+        /// <returns>true ou false</returns>
+        public Boolean SimpleDiv(int val, int div)
+        {
+            return val % div == 0;
+        }
+    }
+}
