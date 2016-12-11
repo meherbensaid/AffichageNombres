79b9f052329081477dcbf35800f0cd711d4bd86f
Meher ben said <Meher ben said>
2016-12-11 20:08:48 +0100

    Test Divisibilite Multiple

--- AffichageNombreTest/UnitTest1.cs
+++ AffichageNombreTest/UnitTest1.cs
@@ -35,5 +35,21 @@ namespace AffichageNombreTest
             Boolean expected = true;
             Assert.AreEqual(expected, C.SimpleDiv(val, 7));
         }
+
+        /// <summary>
+        /// Tester la divisiblité multiple exemple 15 est divisible par 3 et 5 donc on doit 
+        /// avoir FooBar car 15 et divisible par 3(devient Foo) et 5(devient Bar) en tenant en compte l'ordre 3,5,7 
+        /// </summary>
+        [TestMethod]
+        public void DivisibiliteMultiple()
+        {
+            Nombre C = new Nombre();
+
+            int val = 15;
+            String expected = "FooBar";
+            Assert.AreEqual(expected, C.divisibilité(val));
+        }
+
+
     }
 }
--- AffichageNombres/Nombre.cs
+++ AffichageNombres/Nombre.cs
@@ -32,5 +32,65 @@ namespace AffichageNombres
         {
             return val % div == 0;
         }
+
+
+
+        /// <summary>
+        /// methode qui retourne l'quivalent d'un chiffre soit Foo ,soit Bar ,soit Qix en fonction du parametre val
+        /// </summary>
+        /// <param name="val"></param>
+        /// <returns>chaine de caractére</returns>
+        public String chek(int val)
+        {
+            if (val == 3) return "Foo";
+            if (val == 5) return "Bar";
+            if (val == 7) return "Qix";
+            return "";
+        }
+        /// <summary>
+        /// une methode qui retourne la transofrmation d'un chiffre s'il est divisible par 3 et(ou) 5 et(ou) 7
+        /// en tenant compte de l'ordre de divisibilité 3,5 et7
+        /// </summary>
+        /// <param name="val">le nombre à evalué</param>
+        /// <returns>chaine de caractére </returns>
+        /// 
+        public String divisibilité(int val)
+        {
+            Boolean exist = false;
+
+            this.nombre = "";
+            int[] T = new int[3] { 3, 5, 7 };
+            Boolean[] TMP = new Boolean[3] { false, false, false };
+
+            for (int i = 0; i < T.Length && !TMP[i]; i++)
+            {
+                if (SimpleDiv(val, T[i]))
+                {
+                    exist = true;
+                    TMP[i] = true;
+                    nombre += chek(T[i]);
+                    for (int j = 0; j < T.Length; j++)
+                    {
+                        if (i != j)
+                        {
+                            if (SimpleDiv(val, T[j]))
+                            {
+
+                                nombre += chek(T[j]);
+                                TMP[j] = true;
+                            }
+
+                        }
+                    }
+
+                }
+
+            }
+            if (!exist)
+            {
+                nombre += val;
+            }
+            return nombre;
+        }
     }
 }
